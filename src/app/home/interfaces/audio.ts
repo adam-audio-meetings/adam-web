6f3fcf7f1f31dbf3eb3d19d9cd8a8574a0e69227
Rodrigo de Avila Palermo <rodrigo.palermo@fundatec.org.br>
2021-05-11 04:58:38 -0300

    testes blob para file com envio para api formidable

--- src/app/home/interfaces/audio.ts
+++ src/app/home/interfaces/audio.ts
@@ -1,11 +1,12 @@
 import { User } from '../../users/interfaces/user';
 import { Team } from '../../teams/interfaces/team';
+import { Binary } from 'bson';
 
 export interface Audio {
-    _id: string,
+    _id?: string,
     name: string,
-    created_at: Date,
-    file: Blob,
+    created_at: Date
+    file: any | Binary,
     team?: Team,
     member?: User,
 }
