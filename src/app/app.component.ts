584b0817c9bd59d00cca0d441b86128daba7f947
Rodrigo de Avila Palermo <rodrigo.palermo@fundatec.org.br>
2021-06-28 21:46:55 -0300

    Remove firestore imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { interval, Observable } from 'rxjs';
 import { AuthService } from './auth/auth.service';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { AngularFireStorage } from '@angular/fire/storage';
 import { Router } from '@angular/router';
 import { UserService } from './users/user.service';
 import { User } from './users/interfaces/user';
@@ -28,14 +26,11 @@ export class AppComponent implements OnInit {
 
 
   constructor(
-    // firestore: AngularFirestore,
-    // private storage: AngularFireStorage,
     public authService: AuthService,
     public userService: UserService,
     public router: Router
   ) {
 
-    // this.roles$ = firestore.collection('roles').valueChanges({ idField: '_id' });
 
     // const ref = this.storage.ref('github.png');
     // this.imageUrl = ref.getDownloadURL();
